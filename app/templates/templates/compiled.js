e65451e528b63977cf7e684a74c6634df009a094
metaraine <raineorshine@gmail.com>
2014-08-16 12:57:54 -0600

    Precompile initial template so user does not have to run gulp templates.

--- app/templates/templates/compiled.js
+++ app/templates/templates/compiled.js
@@ -1,6 +1,8 @@
-this["Handlebars"] = this["Handlebars"] || {};
-this["Handlebars"]["templates"] = this["Handlebars"]["templates"] || {};
-this["Handlebars"]["templates"]["index"] = {"compiler":[5,">= 2.0.0"],"main":function(depth0,helpers,partials,data) {
+Template = {};
+Template.render = function(templateName, context) { return Handlebars.template(Template.templates[templateName])(context) };
+this["Template"] = this["Template"] || {};
+this["Template"]["templates"] = this["Template"]["templates"] || {};
+this["Template"]["templates"]["index"] = {"compiler":[5,">= 2.0.0"],"main":function(depth0,helpers,partials,data) {
   var helper, functionType="function", escapeExpression=this.escapeExpression;
   return "Hi "
     + escapeExpression(((helper = helpers.city || (depth0 && depth0.city)),(typeof helper === functionType ? helper.call(depth0, {"name":"city","hash":{},"data":data}) : helper)))
